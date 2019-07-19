95d7b27566577562a0418bf90bb34ce7024aeaa9
javadb <52555282+leeseongjong@users.noreply.github.com>
2019-07-19 17:25:21 +0900

    Create BookArray.java

---
+++ javabasic/src/chapter07/BookArray.java
@@ -0,0 +1,29 @@
+package chapter07;
+
+public class BookArray {
+
+	public static void main(String[] args) {
+		Book[] library = new Book[5];
+		Book[] nlibrary = new Book[10];
+		
+		library[0] = new Book("태백산맥", "조정래");
+		library[1] = new Book("데미안", "헤르만 헤세");
+		library[2] = new Book("어떻게 살 것인가", "유시민");
+		library[3] = new Book("토지", "박경리");
+		library[4] = new Book("어린왕자", "생떽쥐페리");
+		
+		for(int i = 0; i < library.length; i++) {
+			library[i].showBookInfo();
+		}
+		
+		for(int i = 0; i < library.length; i++) {
+			System.out.println(library[i]);
+		}
+		
+		System.arraycopy(library, 0, nlibrary, 0, 5);
+		for (int i = 0; i < nlibrary.length; i++) {
+			if(nlibrary[i] != null)nlibrary[i].showBookInfo();
+		}
+	}
+
+}
