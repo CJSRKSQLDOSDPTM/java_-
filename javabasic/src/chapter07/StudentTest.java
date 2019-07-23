0782bc26b871eb25db0927e91dba8634e28aea43
SW Coding School <swcodingschool@gmail.com>
2019-07-24 00:01:43 +0900

    2019-07--24

---
+++ javabasic/src/chapter07/StudentTest.java
@@ -0,0 +1,15 @@
+/*
+ * 07-4 배열 응용 프로그램
+ * p226 예제 시나리오, ArrayList를 사용해 학생 성적 출력 프로그램을 구성
+ * 학생 두 명을 생성하고 각 학생의 과목별 성적과 총점을 출력해보자.
+ */
+package chapter07;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ javabasic/src/chapter07/Subject.java
@@ -0,0 +1,10 @@
+/*
+ * 07-4 배열 응용 프로그램
+ * p226 예제 시나리오, ArrayList를 사용해 학생 성적 출력 프로그램을 구성
+ * 과목 객체를 구현하기 위한 과목 클래스 구현
+ */
+package chapter07;
+
+public class Subject {
+
+}
